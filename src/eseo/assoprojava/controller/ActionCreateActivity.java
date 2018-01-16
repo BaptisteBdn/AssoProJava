57e968211ede1ba98b236e76aaa179c8be844c61
Calvin Ageneau <calvin.ageneau@reseau.eseo.fr>
2018-01-16 10:33:19 +0100

    Modified Organiser and ActionCreateActivity

--- src/eseo/assoprojava/controller/ActionCreateActivity.java
+++ src/eseo/assoprojava/controller/ActionCreateActivity.java
@@ -1,5 +1,21 @@
 package eseo.assoprojava.controller;
 
-public class ActionCreateActivity {
+import java.awt.event.ActionEvent;
 
+public class ActionCreateActivity extends javax.swing.AbstractAction  {
+
+	private static final long serialVersionUID = 1L;
+	public static final String NOM_ACTION = "Créer une activité";
+	
+	public ActionCreateActivity(){
+		super();
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
